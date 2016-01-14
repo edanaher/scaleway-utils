21f3396a54c3f9b2a458b22661927448b138422d
Evan Danaher <edanaher@edanaher.net>
2016-01-14 11:31:39 -0500

    nixos: fix whitespace.

--- nixos/config.nix
+++ nixos/config.nix
@@ -1,22 +1,22 @@
 {
-    platform = {
-      name = "scaleway-c1";
-      kernelMajor = "2.6";
-      kernelHeadersBaseConfig = "defconfig";
-      kernelBaseConfig = "defconfig";
-      kernelArch = "arm";
-      kernelAutoModules = false;
-      kernelExtraConfig =
-        ''
-        '';
-      kernelTarget = "zImage";
-      uboot = null;
-      gcc = {
-        arch = "armv7-a";
-        fpu = "vfpv3";
-        float = "hard";
-      };
+  platform = {
+    name = "scaleway-c1";
+    kernelMajor = "2.6";
+    kernelHeadersBaseConfig = "defconfig";
+    kernelBaseConfig = "defconfig";
+    kernelArch = "arm";
+    kernelAutoModules = false;
+    kernelExtraConfig =
+      ''
+      '';
+    kernelTarget = "zImage";
+    uboot = null;
+    gcc = {
+      arch = "armv7-a";
+      fpu = "vfpv3";
+      float = "hard";
     };
-    build-cores = 4;
-    build-max-jobs = 4;
+  };
+  build-cores = 4;
+  build-max-jobs = 4;
 }
--- nixos/install.sh
+++ nixos/install.sh
@@ -8,65 +8,65 @@ set -ex
 MOUNTPOINT=/mnt/new
 
 function install_nix() {
-	# nix deps
-	apt-get update
-	apt-get install -y --force-yes libbz2-dev libsqlite3-dev libcurl4-openssl-dev libdbd-sqlite3-perl libwww-curl-perl g++ sqlite3 pkg-config patch git
+  # nix deps
+  apt-get update
+  apt-get install -y --force-yes libbz2-dev libsqlite3-dev libcurl4-openssl-dev libdbd-sqlite3-perl libwww-curl-perl g++ sqlite3 pkg-config patch git
 
-	# nix itself
-	wget -c http://nixos.org/releases/nix/nix-1.10/nix-1.10.tar.xz
-	tar -xvf nix-1.10.tar.xz
+  # nix itself
+  wget -c http://nixos.org/releases/nix/nix-1.10/nix-1.10.tar.xz
+  tar -xvf nix-1.10.tar.xz
   (
-		cd nix-1.10
-		./configure
-		make -j 4 # Change 4 to the number of cores your system has.
-		# Wait 3 minutes.
-		make install
+    cd nix-1.10
+    ./configure
+    make -j 4 # Change 4 to the number of cores your system has.
+    # Wait 3 minutes.
+    make install
   )
 
-	# add users
-	getent group nixbld > /dev/null || groupadd -r nixbld
-	for n in $(seq 1 10); do
+  # add users
+  getent group nixbld > /dev/null || groupadd -r nixbld
+  for n in $(seq 1 10); do
       getent passwd nixbld$n && continue
       useradd -c "Nix build user $n" \
-	    -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(which nologin)" \
-		    nixbld$n
+      -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(which nologin)" \
+        nixbld$n
    done
 }
 
 function setup_channel() {
-	#nix-channel --add https://nixos.org/channels/nixos-unstable
-	#nix-channel --update
-	[ -d nixpkgs ] || git clone https://github.com/NixOS/nixpkgs.git /nixpkgs
+  #nix-channel --add https://nixos.org/channels/nixos-unstable
+  #nix-channel --update
+  [ -d /nixpkgs ] || git clone https://github.com/NixOS/nixpkgs.git /nixpkgs
 }
 
 export NIXOS_CONFIG=/etc/nixos/configuration.nix
 function install_installers() {
-	mkdir -p /etc/nixos
-	cp configuration.nix /etc/nixos
+  mkdir -p /etc/nixos
+  cp configuration.nix /etc/nixos
 
-	nix-env -i -K \
-	  -j 4 --cores 4 -f "/nixpkgs/nixos" \
-	  -A config.system.build.nixos-install \
-	  -A config.system.build.nixos-option \
-	  -A config.system.build.nixos-generate-config 
+  nix-env -i -K \
+    -j 4 --cores 4 -f "/nixpkgs/nixos" \
+    -A config.system.build.nixos-install \
+    -A config.system.build.nixos-option \
+    -A config.system.build.nixos-generate-config 
 }
 
 function install_nixos() {
-	[ -L $HOME/.nix-profile ] || ln -s /nix/var/nix/profiles/default/ $HOME/.nix-profile
-	export NIX_LINK="$HOME/.nix-profile"
-	export PATH=$NIX_LINK/bin:$NIX_LINK/sbin:$PATH
-	mkdir -p ${MOUNTPOINT}/nixpkgs
-	mount --bind /nixpkgs ${MOUNTPOINT}/nixpkgs
+  [ -L $HOME/.nix-profile ] || ln -s /nix/var/nix/profiles/default/ $HOME/.nix-profile
+  export NIX_LINK="$HOME/.nix-profile"
+  export PATH=$NIX_LINK/bin:$NIX_LINK/sbin:$PATH
+  mkdir -p ${MOUNTPOINT}/nixpkgs
+  mount --bind /nixpkgs ${MOUNTPOINT}/nixpkgs
 
-	mkdir -p `dirname ${MOUNTPOINT}/$NIXOS_CONFIG`
-	cp configuration.nix ${MOUNTPOINT}/$NIXOS_CONFIG
-	# This is necessary for some reason.
-	# From https://botbot.me/freenode/nixos/2015-05-07/?page=9
-	echo 0 > /proc/sys/vm/mmap_min_addr
-	nixos-install --root ${MOUNTPOINT} \
-	  -j 4 --cores 4
-	mkdir ${MOUNTPOINT}/root/.nixpkgs
-	cp config.nix ${MOUNTPOINT}/root/.nixpkgs/config.nix
+  mkdir -p `dirname ${MOUNTPOINT}/$NIXOS_CONFIG`
+  cp configuration.nix ${MOUNTPOINT}/$NIXOS_CONFIG
+  # This is necessary for some reason.
+  # From https://botbot.me/freenode/nixos/2015-05-07/?page=9
+  echo 0 > /proc/sys/vm/mmap_min_addr
+  nixos-install --root ${MOUNTPOINT} \
+    -j 4 --cores 4
+  mkdir ${MOUNTPOINT}/root/.nixpkgs
+  cp config.nix ${MOUNTPOINT}/root/.nixpkgs/config.nix
 }
 
 
